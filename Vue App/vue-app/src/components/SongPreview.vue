df53f7a18041a2575cf33d22714e18ba11b93132
kpalaciosm02 <60985460+kpalaciosm02@users.noreply.github.com>
2023-05-19 19:09:36 -0600

    Song preview ready. Home view shows list of items but the filters are not ready.

--- Vue App/vue-app/src/components/SongPreview.vue
+++ Vue App/vue-app/src/components/SongPreview.vue
@@ -24,12 +24,57 @@
 
 <template>
     <div class="songPreviewContainer">
-        <p class="songName">{{ songName }}</p>
-        <p class="songAuthor">{{ songAuthor }}</p>
-        <p class="songDuration">{{ songDuration }}</p>
+        <div class="left">
+           <p class="songName">{{ songName }}</p>
+        <p class="songAuthor">{{ songAuthor }}</p> 
+        </div>
+        <div class="right">
+            <p class="songDuration">{{ songDuration }}</p>
+        </div>
     </div>
 </template>
 
 <style>
-
+    .songPreviewContainer{
+        display: flex;
+        border: 1px solid #bbe1fa;
+        border-radius: 4px;
+        padding: 8px 16px;
+        height: auto;
+        width: 480px;
+    }
+    .songName{
+        font-family: 'Helvetica';
+        font-size: 20px;
+        line-height: 20px;
+        color: #bbe1fa;
+        margin-bottom: 4px;
+    }
+    .songAuthor{
+        font-family: 'Helvetica';
+        font-size: 16px;
+        line-height: 16px;
+        color: #bbe1fa
+    }
+    .songDuration{
+        font-family: 'Helvetica';
+        color: #bbe1fa;
+        font-size: 12px;
+        line-height: 12px;
+        float: right;
+    }
+    .left{
+        display: grid;
+        width: 80%;
+        justify-content: left;
+        text-align: left;
+    }
+    .right{
+        width: 20%;
+        float: right;
+        display: flex;
+        align-items: center;
+        text-align: right;
+        justify-content: right;
+    }
 </style>
--- Vue App/vue-app/src/views/HomeView.vue
+++ Vue App/vue-app/src/views/HomeView.vue
@@ -8,7 +8,10 @@
     data(){
       return {
         songs : [
-          { songName: 'TestName', songDuration: '03:00', songAuthor: 'TestAuthor'}
+          { songName: 'Somebody that I used to know', songDuration: '04:03', songAuthor: 'Gotye'},
+          { songName: 'Lose Yourself', songDuration: '05:24', songAuthor: 'Eminem'},
+          { songName: 'Eye of the Beholder', songDuration: '06:26', songAuthor: 'Metallica'},
+          { songName: 'Your Love', songDuration: '04:13', songAuthor: 'The Outfield'}
         ]
       }
     }
@@ -16,9 +19,69 @@
 </script>
 
 <template>
-  <SongPreview songName="Shape of you" songAuthor="Ed Sheeran" songDuration="03:45"></SongPreview>
+  <div class="bigContainer">
+    <div class="songsContainer">
+      <div v-for="song in songs" class="songContainer">
+        <SongPreview :songName="song.songName" :songAuthor="song.songAuthor" :songDuration="song.songDuration"></SongPreview>
+      </div>  
+    </div>
+    
+    <div class="filtersContainer">
+      <label for="filterName" class="filterName">Seleccione un tipo de filtro</label>
+      <select name="filterName" id="filterName" class="filterSelect">
+        <option value="cancion">Canción</option>
+        <option value="artista">Artista</option>
+        <option value="lyrics">Lyrics</option>
+        <option value="genero">Género</option>
+        <option value="lenguaje">Lenguaje</option>
+        <option value="popularidad">Popularidad</option>
+      </select>
+      <button class="filterButton">+ Agregar filtro</button>
+    </div>
+  </div>
+  
 </template>
   
 <style>
-
+  .bigContainer{
+    width: 100%;
+    height: auto;
+    padding-top: 24px;
+    display: flex;
+  }
+  .songsContainer{
+    width: 50%;
+    float: left;
+  }
+  .songContainer{
+    height: auto;
+    float: left;
+    margin-bottom: 4px;
+  }
+  .filtersContainer{
+    width: 50%;
+    float: right;
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+  .filterName{
+    color: #bbe1fa;
+    font-size: 16px;
+  }
+  .filterSelect{
+    background-color: #181818;
+    border: 1px solid #bbe1fa;
+    border-radius: 4px;
+    color: #bbe1fa;
+    margin-left: 4px;
+  }
+  .filterButton{
+    border: 1px solid #bbe1fa;
+    border-radius: 4px;
+    font-size: 14px;
+    cursor: pointer;
+    background-color: #181818;
+    color: #bbe1fa;
+    margin-left: 4px;
+  }
 </style>
