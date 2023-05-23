6529b5a1d105502095ef1f838cdb29068c8cd599
kpalaciosm02 <60985460+kpalaciosm02@users.noreply.github.com>
2023-05-23 01:39:02 -0600

    Homeview almost ready.

--- Vue App/vue-app/src/views/HomeView.vue
+++ Vue App/vue-app/src/views/HomeView.vue
@@ -12,7 +12,30 @@
           { songName: 'Lose Yourself', songDuration: '05:24', songAuthor: 'Eminem'},
           { songName: 'Eye of the Beholder', songDuration: '06:26', songAuthor: 'Metallica'},
           { songName: 'Your Love', songDuration: '04:13', songAuthor: 'The Outfield'}
+        ],
+        filters: [
+
         ]
+      }
+    },
+    methods: {
+      agregarFiltro(event){
+        const selectedFilter = document.getElementById('filterName').value;
+        if (!this.filters.includes(selectedFilter)){
+          this.filters.push(selectedFilter);
+        }
+        else{
+          alert("Este filtro ya se encuentra en el sistema.")
+        }
+      },
+      deleteFilter(filter){
+        const index = this.filters.indexOf(filter);
+        if (index != -1){
+          this.filters.splice(index, 1);
+        }
+        else{
+          alert("El filtro a eliminar no existe.");
+        }
       }
     }
   }
@@ -29,14 +52,21 @@
     <div class="filtersContainer">
       <label for="filterName" class="filterName">Seleccione un tipo de filtro</label>
       <select name="filterName" id="filterName" class="filterSelect">
-        <option value="cancion">Canción</option>
-        <option value="artista">Artista</option>
-        <option value="lyrics">Lyrics</option>
-        <option value="genero">Género</option>
-        <option value="lenguaje">Lenguaje</option>
-        <option value="popularidad">Popularidad</option>
+        <option value="Canciones">Número de canciones</option>
+        <option value="Artista">Artista</option>
+        <!--<option value="lyrics">Lyrics</option>-->
+        <option value="Género">Género</option>
+        <option value="Lenguaje">Lenguaje</option>
+        <option value="Popularidad">Popularidad</option>
       </select>
-      <button class="filterButton">+ Agregar filtro</button>
+      <button class="filterButton" @click="agregarFiltro">+ Agregar filtro</button>
+      <div v-for="filter in filters" class="filter">
+        <div class="filterContainer">
+          {{ filter }}
+          <input type="text" placeholder="Inserte el dato a buscar con el filtro" class="filterInput">
+        </div>
+        <div class="deleteButton" @click="deleteFilter(filter)">-</div>
+      </div>
     </div>
   </div>
   
@@ -49,25 +79,30 @@
     padding-top: 24px;
     display: flex;
   }
+
   .songsContainer{
     width: 50%;
     float: left;
   }
+
   .songContainer{
     height: auto;
     float: left;
     margin-bottom: 4px;
   }
+
   .filtersContainer{
     width: 50%;
     float: right;
     padding-left: 16px;
     padding-right: 16px;
   }
+
   .filterName{
     color: #bbe1fa;
     font-size: 16px;
   }
+
   .filterSelect{
     background-color: #181818;
     border: 1px solid #bbe1fa;
@@ -75,6 +110,7 @@
     color: #bbe1fa;
     margin-left: 4px;
   }
+
   .filterButton{
     border: 1px solid #bbe1fa;
     border-radius: 4px;
@@ -83,5 +119,55 @@
     background-color: #181818;
     color: #bbe1fa;
     margin-left: 4px;
+  }
+  
+  .filter{
+    display: flex;
+    width: 100%;
+    height: auto;
+    margin-left: 50px;
+    margin-top: 12px;
+  }
+
+  .filterContainer{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 24px;
+    width: 80%;
+    border: 1px solid #bbe1fa;
+    border-radius: 4px;
+    color: #bbe1fa;
+    font-size: 16px;
+    margin-top: 4px;
+    padding-left: 6px;
+  }
+
+  .deleteButton{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 24px;
+    width: 5%;
+    border: 1px solid #bbe1fa;
+    border-radius: 4px;
+    color: #bbe1fa;
+    font-size: 16px;
+    margin-left: 40px;
+    margin-top: 4px;
+    cursor: pointer;
+  }
+
+  .filterInput{
+    border: 1px solid #bbe1fa;
+    border-radius: 4px;
+    font-size: 14px;
+    background-color: #181818;
+    color: #bbe1fa;
+    width: 75%;
+  }
+
+  .filterInput::placeholder{
+    color: #bbe1fa;
   }
 </style>
